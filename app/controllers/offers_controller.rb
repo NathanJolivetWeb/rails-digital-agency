c4c5fc58b8645a98505637c091747cbca8b1b8d9
Rzt Prog <rztprog@outlook.fr>
2021-08-18 14:38:45 +0200

    Add update action to offer

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,6 @@
 class OffersController < ApplicationController
-  
+  before_action :offer_id, only: %i[show edit delete update]
+
   def show
     @offer = Offer.find(params[:id])
     @booking = Booking.new
@@ -27,7 +28,24 @@ class OffersController < ApplicationController
     end
   end
 
+  def edit
+    if (@offer.user != current_user)
+      redirect_to offers_path
+    end
+  end
+  
+  def update
+    if (@offer.user == current_user)
+      @offer.update(offer_params)
+    end
+    redirect_to offers_path
+  end
+
   private
+
+  def offer_id
+    @offer = Offer.find(params[:id])
+  end
 
   def offer_params
     params.require(:offer).permit(:title, :description, :price_per_day)
