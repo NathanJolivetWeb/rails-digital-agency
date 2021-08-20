1e00cd786defe3a28e3dd25ebf28c5e7e91ab138
Nathan Jolivet <nathan.jolivet@gmail.com>
2021-08-20 14:12:06 +0200

    conflictsolve

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,8 +1,6 @@
 class Offer < ApplicationRecord
   belongs_to :user
-  has_many :bookings
-  has_many :reviews, through: :bookings
   has_many :bookings, dependent: :destroy
-
+  has_many :reviews, through: :bookings
   validates :category, inclusion: { in: ["front-end", "back-end", "ux-ui"] }
 end
