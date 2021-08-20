9c13d11a3ff3cf5139d69c3641f8ff9410b6b091
Christophe Becquey <christophe.becquey@laposte.net>
2021-08-20 12:32:42 +0200

    add review + rating

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,6 +1,7 @@
 class Offer < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_many :reviews, through: :bookings
 
   validates :category, inclusion: { in: ["front-end", "back-end", "ux-ui"] }
 end
